83acf371d512bdab929a15e4b4bc64368509e63e
邱姵瑄 <ymes050623@gmail.com>
2021-05-30 20:24:25 +0800

    20210530

--- datacenter/datacenter/settings.py
+++ datacenter/datacenter/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-84x#+p)iw161-$wtla^@3!-pjjp)-h^bssn4a#b_(vr=&j)cj+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'datacenter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
@@ -116,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
--- datacenter/mysite/views.py
+++ datacenter/mysite/views.py
@@ -1,3 +1,21 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+from mysite.models import Post
 
-# Create your views here.
+def index(request):
+	posts = Post.objects.all()
+	myname = "降雨量"
+	data = [i for i in range(1,43)]
+	random.shuffle(data)
+	lotto_numbers = data[0:6]
+	special_number = data[6]
+	return render(request,'index.html',locals())
+
+def show(request, id):
+	try:
+		target = Post.objects.get(id=id)
+	except:
+		return redirect("/")
+	return render(request,"showpost.html", locals())
